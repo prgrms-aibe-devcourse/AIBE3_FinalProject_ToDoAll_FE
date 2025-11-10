a93e443780ab74dfc21ba93ca1c007b9ee813250
JAESEUNGJAE <valkyrie9923@gmail.com>
2025-11-10 11:36:40 +0900

    [feat] JD 생성 페이지 구현

--- src/pages/JDCreatePage.tsx
+++ src/pages/JDCreatePage.tsx
@@ -1,3 +1,13 @@
+import JobPostForm from '../features/jd/components/form/JobPostForm';
+import type { JobPostFormValues } from '../features/jd/components/form/JobPostForm';
+
 export default function JDCreatePage() {
-  return <div>JD Create Page</div>;
+  const handleSubmit = (v: JobPostFormValues) => {
+    alert('제출 값: ' + JSON.stringify(v, null, 2));
+  };
+  return (
+    <main className="min-h-screen bg-gray-50 p-4 sm:p-6">
+      <JobPostForm onSubmit={handleSubmit} />
+    </main>
+  );
 }
