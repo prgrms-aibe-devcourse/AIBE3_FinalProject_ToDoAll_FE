f33a5d61f352af5fc5acbe3e3828b64cece360d7
jiyoon-00 <pulip2517@gmail.com>
2025-11-12 10:43:59 +0900

    [feat] 푸터 ui제작

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -1,7 +1,56 @@
 const Footer = () => {
   return (
     <>
-      <footer></footer>
+      <footer
+        className="
+        bg-[#F6EFEF]
+        text-[#413F3F]
+        ring-1 ring-[#E3DBDB]
+        shadow-[0_6px_22px_rgba(0,0,0,.1)]
+        font-[var(--default-font-family)]
+        "
+      >
+        <div className="mx-auto max-w-6xl px-6 py-10 grid grid-cols-1 md:grid-cols-2 gap-6 items-start">
+          {/* 로고 / 좌측 */}
+          <div className="flex items-center justify-start h-full gap-4">
+            <img src="/logo/login-logo.png" alt="jobda" className="h-20 w-auto " />
+          </div>
+
+          {/* 링크 / 우측 */}
+          <div className="flex md:justify-end">
+            <ul className="text-base leading-8">
+              <li className="font-bold mb-2 text-[var(--color-jd-violet)] ">Jobda</li>
+              <li>
+                <a className="text-[var(--color-jd-violet)] transition" href="/dashboard">
+                  대시보드
+                </a>
+              </li>
+              <li>
+                <a className="text-[var(--color-jd-violet)] transition" href="/jobs">
+                  공고 관리
+                </a>
+              </li>
+              <li>
+                <a className="text-[var(--color-jd-violet)] transition" href="/resumes">
+                  지원자 조회
+                </a>
+              </li>
+              <li>
+                <a className="text-[var(--color-jd-violet)] transition" href="/interview/manage">
+                  면접 관리
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        {/* 하단 저작권 영역 */}
+        <div className="border-t border-[#E3DBDB]">
+          <div className="text-[var(--color-jd-violet)] font-semibold mx-auto max-w-6xl px-6 py-5 text-sm text-right">
+            © 2025 <span className="!font-bold">Jobda Inc.</span> All rights reserved.
+          </div>
+        </div>
+      </footer>
     </>
   );
 };
