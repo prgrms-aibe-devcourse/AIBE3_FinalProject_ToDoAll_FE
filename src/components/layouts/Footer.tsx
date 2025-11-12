f8afd82e175b685a9598248e217166caac5d61b4
jiyoon-00 <pulip2517@gmail.com>
2025-11-12 10:51:00 +0900

    [feat] 폰트 사이즈 조절

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -10,10 +10,10 @@ const Footer = () => {
         font-[var(--default-font-family)]
         "
       >
-        <div className="mx-auto max-w-6xl px-6 py-10 grid grid-cols-1 md:grid-cols-2 gap-6 items-start">
+        <div className="mx-auto max-w-6xl px-6 py-6 grid grid-cols-1 md:grid-cols-2 gap-6 items-start">
           {/* 로고 / 좌측 */}
           <div className="flex items-center justify-start h-full gap-4">
-            <img src="/logo/login-logo.png" alt="jobda" className="h-20 w-auto " />
+            <img src="/logo/login-logo.png" alt="jobda" className="h-18 w-auto " />
           </div>
 
           {/* 링크 / 우측 */}
@@ -21,22 +21,25 @@ const Footer = () => {
             <ul className="text-base leading-8">
               <li className="font-bold mb-2 text-[var(--color-jd-violet)] ">Jobda</li>
               <li>
-                <a className="text-[var(--color-jd-violet)] transition" href="/dashboard">
+                <a className="text-sm text-[var(--color-jd-violet)] transition" href="/dashboard">
                   대시보드
                 </a>
               </li>
               <li>
-                <a className="text-[var(--color-jd-violet)] transition" href="/jobs">
+                <a className="text-sm text-[var(--color-jd-violet)] transition" href="/jobs">
                   공고 관리
                 </a>
               </li>
               <li>
-                <a className="text-[var(--color-jd-violet)] transition" href="/resumes">
+                <a className="text-sm text-[var(--color-jd-violet)] transition" href="/resumes">
                   지원자 조회
                 </a>
               </li>
               <li>
-                <a className="text-[var(--color-jd-violet)] transition" href="/interview/manage">
+                <a
+                  className="text-sm text-[var(--color-jd-violet)] transition"
+                  href="/interview/manage"
+                >
                   면접 관리
                 </a>
               </li>
@@ -46,7 +49,7 @@ const Footer = () => {
 
         {/* 하단 저작권 영역 */}
         <div className="border-t border-[#E3DBDB]">
-          <div className="text-[var(--color-jd-violet)] font-semibold mx-auto max-w-6xl px-6 py-5 text-sm text-right">
+          <div className="text-sm text-[var(--color-jd-violet)] font-semibold mx-auto max-w-6xl px-6 py-5 text-sm text-right">
             © 2025 <span className="!font-bold">Jobda Inc.</span> All rights reserved.
           </div>
         </div>
