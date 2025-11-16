953fc549a8e0ae0a2aa3f60f6ad3b5711fad5d0b
AEU <aeuplayground@gmail.com>
2025-11-17 02:16:18 +0900

    [refactor] 공고별 합격 현황 NumberSlotCard 변수 적용

--- src/features/dashboard/NumberSlotCard.tsx
+++ src/features/dashboard/NumberSlotCard.tsx
@@ -1,24 +1,50 @@
-import Badge from '@components/dashboard/Badge.tsx';
+import Badge, { type BadgeType } from '@components/dashboard/Badge.tsx';
 
-function SlotCard() {
+const STATUS_INFO = {
+  document: {
+    text: '서류 접수',
+    color: 'scarlet',
+  },
+  interview: {
+    text: '면접',
+    color: 'violet',
+  },
+  finished: {
+    text: '마감',
+    color: 'gray',
+  },
+} as const satisfies Record<string, { text: string; color: BadgeType }>;
+
+type StatusType = keyof typeof STATUS_INFO;
+
+export type NumberSlotsCardProps = {
+  title: string;
+  slotData: number[];
+  status: StatusType;
+};
+
+function SlotCard({ title, value }: { title: string; value: number }) {
   return (
     <section className="flex flex-col justify-center text-center align-middle">
-      <p className="text-5xl font-bold">19</p>
-      <p className="font-semibold">지원자</p>
+      <p className="text-5xl font-bold">{value}</p>
+      <p className="font-semibold">{title}</p>
     </section>
   );
 }
 
-export default function NumberSlotsCard() {
+export default function NumberSlotsCard({ title, slotData, status }: NumberSlotsCardProps) {
   return (
     <section className="border-jd-gray-light relative flex flex-col items-center gap-2 rounded-[10px] border-1 bg-white p-[20px] sm:items-start">
-      <Badge text="진행중" color="scarlet" className="static top-[20px] right-[20px] sm:absolute" />
-      <h3 className="text-jd-black font-bold">시니어 프론트엔드 개발자</h3>
+      <Badge
+        text={STATUS_INFO[status].text}
+        color={STATUS_INFO[status].color}
+        className="static top-[20px] right-[20px] sm:absolute"
+      />
+      <h3 className="text-jd-black mb-4 font-bold">{title}</h3>
       <section className="mb-[10px] flex w-full flex-row flex-wrap justify-around gap-6 sm:gap-4">
-        <SlotCard />
-        <SlotCard />
-        <SlotCard />
-        <SlotCard />
+        {['지원자', '북마크', '면접', '합격'].map((item, index) => (
+          <SlotCard key={index} title={item} value={slotData[index]} />
+        ))}
       </section>
       <section className="bg-jd-scarlet h-[10px] w-full rounded-[90px] opacity-25"></section>
     </section>
