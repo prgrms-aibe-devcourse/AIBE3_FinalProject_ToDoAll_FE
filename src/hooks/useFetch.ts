795df5afdd962dac9e415afaea27e49cbd9e1066
AEU <aeuplayground@gmail.com>
2025-11-18 16:48:29 +0900

    [refactor] useFetch 수정

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,4 +1,6 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+
+const baseUrl = import.meta.env.VITE_API_BASE_URL;
 
 interface CommonResponse<T> {
   errorCode?: number;
@@ -14,27 +16,36 @@ export default function useFetch<T>(
   body?: Record<string, any>
 ) {
   const [resData, setResData] = useState<T | null>(defaultValue ?? null);
+  const controller = useRef<AbortController | null>(null);
 
   useEffect(() => {
-    const controller = new AbortController();
-    const signal = controller.signal;
-    fetch('http://localhost:8080/' + url, {
+    controller.current = new AbortController();
+    const signal = controller.current.signal;
+    fetch(baseUrl + url, {
       signal,
       method,
+      credentials: 'include',
       headers: {
         'Content-Type': 'application/json',
+        Authorization: `Bearer ~`,
         ...headers,
       },
       body: JSON.stringify(body),
     })
-      .then((res) => res.json())
+      .then((res) => {
+        if (!res.ok) throw new Error(res.status + ' : ' + '네트워크 응답이 OK가 아님');
+        return res.json();
+      })
       .then((jsonData: CommonResponse<T>) => {
         setResData(jsonData.data ?? null);
       })
-      .catch((err) => console.error(err.errorCode, ' : ', err.message));
+      .catch((err) => {
+        if (err.name === 'AbortError') return;
+        console.error(err.message);
+      });
 
     return () => {
-      controller.abort();
+      if (controller.current) controller.current.abort();
     };
   }, [url, method, headers, body]);
 
