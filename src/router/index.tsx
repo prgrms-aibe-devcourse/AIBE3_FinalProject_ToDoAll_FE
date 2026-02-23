316de6d3147f90dbc66d5a9b1e9a195707b5e2e9
AEU <aeuplayground@gmail.com>
2026-02-23 19:33:16 +0900

    [refactor] NoLayout 추가

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,14 +1,20 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
-import { mainRoutes, noLayoutRoutes } from './routes';
+import { authLayoutRoutes, mainRoutes, noLayoutRoutes } from './routes';
 
 import AuthRouteGuard from '@/router/AuthRouteGuard.tsx';
 import AuthLayout from '@shared/components/layouts/AuthLayout.tsx';
+import NoLayout from '@shared/components/layouts/NoLayout.tsx';
 
 export default function AppRouter() {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/dashboard" replace />} />
       <Route element={<AuthLayout />}>
+        {authLayoutRoutes.map((route) => (
+          <Route key={route.path} path={route.path} element={route.element} />
+        ))}
+      </Route>
+      <Route element={<NoLayout />}>
         {noLayoutRoutes.map((route) => (
           <Route key={route.path} path={route.path} element={route.element} />
         ))}
